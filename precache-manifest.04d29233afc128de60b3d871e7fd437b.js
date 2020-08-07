3301b34ce95a30b69a1e0bc6eb2c282d4c59595f
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-07 16:42:21 +0530

    Updates

---
+++ precache-manifest.04d29233afc128de60b3d871e7fd437b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bf36f47aeef4043a408ef28f9bd2d7",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "6ea3ab1d07e2a0054bd0",
+    "url": "/expense-tracker-react/static/css/main.5d96a794.chunk.css"
+  },
+  {
+    "revision": "30cbb7b387f744052ea5",
+    "url": "/expense-tracker-react/static/js/2.b920bf9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.b920bf9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea3ab1d07e2a0054bd0",
+    "url": "/expense-tracker-react/static/js/main.f8e83aad.chunk.js"
+  },
+  {
+    "revision": "862869cc79fa6dc735cc",
+    "url": "/expense-tracker-react/static/js/runtime-main.bdd3deb4.js"
+  }
+]);
